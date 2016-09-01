86be42d6179cbe7162a3427d6a07abf77f5f7de9
systopia <endres@systopia.de>
2016-09-01 15:15:13 +0200

    JS prefill

--- js/prefill.js
+++ js/prefill.js
@@ -11,4 +11,21 @@
 | written permission from the original author(s).        |
 +--------------------------------------------------------*/
 
-alert("TEST");
+
+(function($) {
+  $(document).ready(function(){
+
+    function setBatchName() {
+      var title      = $("input[name='submitted[registrant_title]']").val();
+      var first_name = $("input[name='submitted[registrant_first_name]']").val();
+      var last_name  = $("input[name='submitted[registrant_last_name]']").val();
+      var badge_name = title + " " + first_name + " " + last_name;
+
+      $("input[name='submitted[registrant_badge]']").val(badge_name.trim());
+    }
+
+    $("input[name='submitted[registrant_title]']").change(setBatchName);
+    $("input[name='submitted[registrant_first_name]']").change(setBatchName);
+    $("input[name='submitted[registrant_last_name]']").change(setBatchName);
+  });
+})(jQuery);
