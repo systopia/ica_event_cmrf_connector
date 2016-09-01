ad995e35743ebc53b4c695f3d408cb1f95d4443a
systopia <endres@systopia.de>
2016-09-01 15:21:34 +0200

    JS prefill

--- js/prefill.js
+++ js/prefill.js
@@ -15,17 +15,28 @@
 (function($) {
   $(document).ready(function(){
 
-    function setBatchName() {
-      var title      = $("input[name='submitted[registrant_title]']").val();
-      var first_name = $("input[name='submitted[registrant_first_name]']").val();
-      var last_name  = $("input[name='submitted[registrant_last_name]']").val();
-      var badge_name = title + " " + first_name + " " + last_name;
-
-      $("input[name='submitted[registrant_badge]']").val(badge_name.trim());
+    function setRegistrantBatchName() {
+      setBatchName("registrant");
     }
 
-    $("input[name='submitted[registrant_title]']").change(setBatchName);
-    $("input[name='submitted[registrant_first_name]']").change(setBatchName);
-    $("input[name='submitted[registrant_last_name]']").change(setBatchName);
+    function setPartnerBatchName() {
+      setBatchName("partner");
+    }
+
+    function setBatchName(prefix) {
+      var title      = $("input[name='submitted[" + prefix + "_title]']").val();
+      var first_name = $("input[name='submitted[" + prefix + "_first_name]']").val();
+      var last_name  = $("input[name='submitted[" + prefix + "_last_name]']").val();
+      var badge_name = title + " " + first_name + " " + last_name;
+
+      $("input[name='submitted[" + prefix + "_badge]']").val(badge_name.trim());
+    }
+
+    $("input[name='submitted[registrant_title]']").change(setRegistrantBatchName);
+    $("input[name='submitted[registrant_first_name]']").change(setRegistrantBatchName);
+    $("input[name='submitted[registrant_last_name]']").change(setRegistrantBatchName);
+    $("input[name='submitted[partner_title]']").change(setPartnerBatchName);
+    $("input[name='submitted[partner_first_name]']").change(setPartnerBatchName);
+    $("input[name='submitted[partner_last_name]']").change(setPartnerBatchName);
   });
 })(jQuery);
